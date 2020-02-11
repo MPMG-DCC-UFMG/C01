f5b49d629d75fe0bac50b028702169578aa23909
LorenaMendes <lorenamendes.contato@gmail.com>
2020-02-11 17:09:33 -0500

    crawler para TRF5

---
+++ TRF5/html_retrieve.py
@@ -0,0 +1,39 @@
+import urllib.request
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException 
+
+url_base = 'http://www5.trf5.jus.br/precatorio/'
+
+model1 = '//*[@id="wrapper"]/h1'
+model2 = '/html/body/p[2]'
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(chrome_options=chrome_options)
+
+# checa se um dado xpath existe
+def check_exists_by_xpath(xpath, url):
+    driver.get(url)
+    try:
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    return True
+
+# URLs de forma sequencial
+i = 1
+while True:
+	url = url_base + str(i)
+	
+	if check_exists_by_xpath(model1, url) or check_exists_by_xpath(model2, url):
+
+		filename = 'precatorio-' + str("{:06d}".format(i)) + '.html'
+		path = './pages/' + filename
+		
+		# guarda o conteúdo html da página acessada
+		urllib.request.urlretrieve(url, path)
+		i += 1
+	else:
+		break
+
