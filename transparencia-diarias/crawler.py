6ba7fb8f54a1044910daf3178c6745a21e0ca0df
LorenaMendes <lorenamendes.contato@gmail.com>
2020-02-13 11:30:27 -0500

    time sleep fix

--- transparencia-diarias/crawler.py
+++ transparencia-diarias/crawler.py
@@ -11,6 +11,7 @@ chrome_options.add_experimental_option('prefs', prefs)
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
 driver.get('http://transparencia.mg.gov.br/estado-pessoal/diarias')
+time.sleep(2)
 
 year_button = '//*[@id="jform_ano"]'
 submit_button = '//*[@id="estado_despesadiarias-form"]/div[7]/div/button'
@@ -20,10 +21,10 @@ csv_button = '//*[@id="DataTables_Table_0_wrapper"]/div[1]/a[2]'
 # relativos aos anos 2002 a 2020
 for i in range(1, 20):
 	driver.find_element_by_xpath(year_button).click()
-	
 	year = '//*[@id="jform_ano"]/option[' + str(i) + ']'
 	driver.find_element_by_xpath(year).click()
 	
 	driver.find_element_by_xpath(submit_button).click()
+	time.sleep(2)
 	driver.find_element_by_xpath(show_all).click()
 	driver.find_element_by_xpath(csv_button).click()
