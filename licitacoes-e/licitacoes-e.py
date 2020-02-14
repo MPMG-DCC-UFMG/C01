2e382008b3ab918a4f43bf35573712226545645a
Luiz Felipe Nery <42241392+luiznery@users.noreply.github.com>
2020-02-14 15:45:00 +0100

    code

--- licitacoes-e/licitacoes-e.py
+++ licitacoes-e/licitacoes-e.py
@@ -1,104 +1,107 @@
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import datetime
 import time
 import os
 
-
 from PIL import Image
 import cv2
 import pytesseract
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
+import speech_recognition as sr
+from os import remove
+from os import path
+from pydub import AudioSegment
 
+
+# Quebra captcha de audio
+def transcribe(audio, limit = 5):
+    sound = AudioSegment.from_mp3(audio)
+    AUDIO_FILE = "audio.wav"
+    sound.export(AUDIO_FILE, format="wav")
+    r = sr.Recognizer()
+    resposta = ""
+    attempts = 0
+    with sr.AudioFile(AUDIO_FILE) as source:
+        while attempts < limit:
+            try:
+                audio = r.record(source)  # read the entire audio file                  
+                resposta = r.recognize_google(audio)
+                attempts = limit + 1
+            except:
+                attempts += 1
+        remove(AUDIO_FILE)
+    return resposta, attempts
+
+#Qubra captcha de imagem
 def solve_captcha(img_path):
     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    # img[img>110] = 240
-    # img[img<30] = 240
-    img[img>53]=250 
+    img[img>110] = 240
+    img[img<30] = 240
     img = img[:,20:150]
     return pytesseract.image_to_string(img)
-
-
-def preenche_form(driver,option):
-    same_page = True
-    while same_page:
-        try:
-            wait = WebDriverWait(driver, 5)
-            wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="licitacaoPesquisaSituacaoForm"]/div[14]/input')))
-        except TimeoutException:
-            return
-
-        try:    
-            situacao = driver.find_element_by_xpath("""//*[@id="licitacaoPesquisaSituacaoForm"]/div[5]/span/input""")
-            situacao.clear()
-            situacao.send_keys(option)
-
-            with open('captcha.png', 'wb') as file:
-                file.write(driver.find_element_by_xpath('//*[@id="img_captcha"]').screenshot_as_png)
-            resposta = solve_captcha('captcha.png')
-
-            while resposta == "":
-                driver.find_element_by_xpath("""//*[@id="licitacaoPesquisaSituacaoForm"]/div[13]/div[2]/img[2]""").click()
-                print("recarregando imagem")
-                time.sleep(1)
-                with open('captcha.png', 'wb') as file:
-                    file.write(driver.find_element_by_xpath('//*[@id="img_captcha"]').screenshot_as_png)
-
-                resposta = solve_captcha('captcha.png')
-
-            print(resposta)
-
-            driver.find_element_by_xpath("""//*[@id="pQuestionAvancada"]""").send_keys(resposta)
-            driver.find_element_by_xpath("""//*[@id="licitacaoPesquisaSituacaoForm"]/div[14]/input""").click()
-            try:
-                if driver.find_element_by_xpath("""//*[@id="msgs_fechar"]""").get_attribute('innerHTML') == X:
-                    same_page = False
-            except:
-                continue
-        except:
-            continue
-            
-            
 
 
 url = "https://www.licitacoes-e.com.br/aop/pesquisar-licitacao.aop?opcao=preencherPesquisar"
 
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
 
 driver = webdriver.Chrome(options=options)
 driver.get(url)
-
 print("Pagina carregada")
-time.sleep(2)
 
-preenche_form(driver, "Publicada")
-print("cabo")
-driver.close()
+# Prenche formulario
+# option = input("Situação da licitação: ")
+option = "Publicada"
+situacao = driver.find_element_by_xpath("""//*[@id="licitacaoPesquisaSituacaoForm"]/div[5]/span/input""")
+situacao.clear()
+situacao.send_keys(option)
 
+#Pede para usuario preencher o captcha
+while True:
+    resposta = input("Captcha: ")
+    driver.find_element_by_xpath("""//*[@id="pQuestionAvancada"]""").send_keys(resposta)
+    driver.find_element_by_xpath("""//*[@id="licitacaoPesquisaSituacaoForm"]/div[14]/input""").click()
+    try: 
+        Select(driver.find_element_by_xpath('//*[@id="tCompradores_length"]/label/select')).select_by_visible_text("Todos")
+        break
+    except:
+        continue
 
+# Para cada linha da tabela
+table =  driver.find_element_by_xpath("""//*[@id="tCompradores"]""")
+for row in table.find_elements_by_xpath(".//tr/td[5]/img"):
     
+    #abrindo nova janela
+    row.click()
+    time.sleep(2)
+    driver.switch_to_window(driver.window_handles[1])
+    
+    # Passando por captcha
+    #clica no botao do captcha
+    time.sleep(2)
+    frame = driver.find_element_by_xpath('//*[@id="html_element"]/div/div/iframe')
+    driver.switch_to.frame(frame)
+    driver.find_element_by_xpath("//*[@id='recaptcha-anchor']").click()
+    driver.switch_to.default_content()
+    #escolhe audio
+    time.sleep(5)
+    frame = driver.find_element_by_xpath('//*[@id="bodyPrincipal"]/div[3]/div[4]/iframe')
+    driver.switch_to.frame(frame)
+    driver.find_element_by_xpath('//*[@id="rc-imageselect"]/div[3]/div[2]/div[1]/div[1]/div[2]').click()
+    driver.switch_to.default_content()
 
+    #recupera dados para aquela tabela
+    # ...   
 
+    # Fecha aba atual
+    driver.close()
+    driver.switch_to_window(driver.window_handles[0])
 
-
-
-
-# situacao.submit()
-
-
-
-# situacao = Select(driver.find_element_by_name('select0'))
-# select.select_by_visible_text("Publicada")
-
-# select = Select(driver.find_element_by_id('situacoes'))
-# select.select_by_visible_text("Publicada")
-
-# select by visible text
-# select.select_by_visible_text('Selecione a situação')
-
+# Fecha janelas
+print("Dados recolhidos")
+time.sleep(5)
+driver.close()
