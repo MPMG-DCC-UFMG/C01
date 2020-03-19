10a39d56ad6617e9ba5013813e5b73cc63f89d04
Rúbia <rubia-rg@users.noreply.github.com>
2020-03-19 13:28:35 -0700

    Fixed access denied issue
    Refactored to use wget + requests instead of selenium

--- processo-compras/compras/config.py
+++ processo-compras/compras/config.py
@@ -1,52 +1,35 @@
 import logging
 
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-
-
 BASE_FOLDER = "/br.gov.mg.compras/processocompra/"
 PROFILE_PATH = "/Users/work/Library/Application Support/Firefox/Profiles/yde1eaqi.mpmg"
 DOWNLOAD_DIR = "/Volumes/Work HD/MPMG"
-PROCESS_NOT_FOUND = 'O(A) "Processo Compra" não pode ser alterado(a), pois foi excluído(a) por outro usuário, ' \
+NO_PERMISSION_MESSAGE = "You don't have permission to access"
+PROCESS_NOT_FOUND_MESSAGE = 'O(A) "Processo Compra" não pode ser alterado(a), pois foi excluído(a) por outro usuário, ' \
                     'em acesso concorrente, enquanto esta tela era visualizada.'
-
 RELATORIO_DETALHES = 'https://www1.compras.mg.gov.br/processocompra/processo/relatoriodetalhes/' \
-      'relatorioDetalhesProcessoCompra.html?id='
+                     'relatorioDetalhesProcessoCompra.html?id='
 MAX_RETRIES = 10
 WAIT_INTERVAL = 30
-START_PROCESS = 3030
+START_PROCESS = 3038
 MAX_PROCESSES = 1000000
 MAX_SKIPPED_IDS = 5000
 BASE = "https://www1.compras.mg.gov.br/processocompra/processo/visualizacaoArquivosProcesso.html?id="
-FILE_EXTENSIONS = [".pdf", ".doc", ".docx", ".zip", ".odt", ".eml"]
-MAX_THREADS = 4
+FORM_DATA = {"idArquivo": '', "nomeArquivo": '', "chaveAberturaArquivo": ''}
+USER_AGENT_LIST = [
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
+]
 
-
-def set_options():
-    firefox_options = Options()
-    firefox_options.add_argument("--headless")
-    firefox_options.add_argument("--window-size=1920x1080")
-    firefox_options.add_argument("--disable-notifications")
-    firefox_options.add_argument('--no-sandbox')
-    firefox_options.add_argument('--verbose')
-    firefox_options.add_argument('--disable-gpu')
-    firefox_options.add_argument('--disable-software-rasterizer')
-    firefox_options.set_preference("browser.download.manager.showWhenStarting", False)
-    firefox_options.set_preference('browser.helperApps.neverAsk.openFile', "application/pdf")
-    firefox_options.set_preference('browser.helperApps.neverAsk.saveToDisk', "application/pdf")
-    firefox_options.set_preference('browser.helperApps.neverAsk.openFile', "application/zip")
-    firefox_options.set_preference('browser.helperApps.neverAsk.saveToDisk', "application/zip")
-    firefox_options.set_preference('browser.helperApps.neverAsk.openFile', "application/msword")
-    firefox_options.set_preference('browser.helperApps.neverAsk.saveToDisk', "application/msword")
-    firefox_options.set_preference("pdfjs.disabled", "true")
-    return firefox_options
-
-
-def get_driver(profile_path=PROFILE_PATH):
-    firefox_options = set_options()
-    driver = webdriver.Firefox(firefox_profile=profile_path,
-                               options=firefox_options)
-    return driver
+STRING_PATTERN = '\(([^)]+)\)'
+JS_METHOD = 'visualizarArquivo'
 
 
 def set_logging():
