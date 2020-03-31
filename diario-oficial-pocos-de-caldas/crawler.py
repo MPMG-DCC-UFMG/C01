ac824d3fea7a62387bf1e190110e3a36a877c9b1
Lucas Augusto <luc.aug.freire@gmail.com>
2020-03-31 01:44:43 -0300

    Atualiza coletor Diário Oficial de Poços de Caldas

--- diario-oficial-pocos-de-caldas/crawler.py
+++ diario-oficial-pocos-de-caldas/crawler.py
@@ -5,7 +5,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
