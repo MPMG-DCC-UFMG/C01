eff9cc1b4137b239056e112470d264e310887a4f
gabrielcrds <gabriel.d.a.cardoso@gmail.com>
2020-04-06 10:02:10 -0300

    Add base for chorme-extension

---
+++ chrome-extension/devtools.js
@@ -0,0 +1,7 @@
+chrome.devtools.panels.elements.createSidebarPane(
+    "Mister Crawler",
+    function (sidebar) {
+        sidebar.setPage("sidebar/sidebar.html");
+        // sidebar.setHeight("8ex");
+    }
+);
