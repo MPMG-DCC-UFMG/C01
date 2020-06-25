99efdc44ff7d81e49fb45bb84f02f95aef6a6a13
Lucas Augusto <luc.aug.freire@gmail.com>
2020-06-25 20:18:11 -0300

    Remove exportação de arquivos desnecessários

--- range_inference/range_inference/__init__.py
+++ range_inference/range_inference/__init__.py
@@ -2,4 +2,4 @@
 This module filters the search space for a specified parameter using a binary
 search based heuristic
 """
-from range_inference.range_inference import *
+from range_inference.range_inference import RangeInference
