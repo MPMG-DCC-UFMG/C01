59ceb334b47a7b00730b03dea01aa2bf42950397
TalesPanoutsos <tales.panoutsos@gmail.com>
2020-06-30 15:05:14 -0300

    organizing files

---
+++ step-by-step/tests/crawler-tests/diario-oficial-uniao/diario_oficial_uniao_steps.py
@@ -0,0 +1,8 @@
+import sys 
+sys.path.append("code")
+from functions_file import *
+
+async def execute_steps(**missing_argument):
+    await clique(**{'xpath': '//*[@id="banner"]/div[1]/div[2]/div[1]/div/div/ul/li[3]/a'}, **missing_argument)
+    await clique(**{'xpath': '//*[@id="portlet_leituradou_INSTANCE_GKE7NRmaCXgl"]/div/div[2]/div/div[1]/div[3]/button[4]'}, **missing_argument)
+    espere(**{'segs': 3})
