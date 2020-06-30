e105fb22aa96cd4ea4f25840196aa2b59ef88e49
Rennan Cordeiro <rennancl@gmail.com>
2020-06-30 19:22:38 -0300

    select and select options step done

--- step-by-step/code/functions_file.py
+++ step-by-step/code/functions_file.py
@@ -7,9 +7,6 @@ def range_(stop):
 def print_(word):
     return word
 
-def selecione(driver, xpath_select, opcao):
-    return None
-
 async def clique(page, xpath):
     await page.waitForXPath(xpath)
     elements = await page.xpath(xpath)
@@ -17,6 +14,21 @@ async def clique(page, xpath):
         elements[0].click(),
         page.waitForNavigation(),
     ])
+
+async def opcoes(page, xpath):
+    option_values = []
+    await page.waitForXPath(xpath)
+    options = await page.xpath(xpath + "/option")
+    for option in options:
+        value = await option.getProperty("text")
+        option_values.append(value.toString().split(":")[-1])
+    return option_values
+
+async def selecione(page, xpath, opcao):
+    await page.waitForXPath(xpath)
+    elements = await page.xpath(xpath)
+    select = elements[0]
+    await select.type(opcao)
 
 
 async def elementos_nesse_xpath(page, xpath):
