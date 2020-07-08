ba429e57cb2a2f6ab6ded1e667a4ae1568256eec
Rennan Cordeiro <rennancl@gmail.com>
2020-07-08 15:27:00 -0300

    removing intermediate file and load code in memory

--- step-by-step/tests/crawler-tests/cnes/main.py
+++ step-by-step/tests/crawler-tests/cnes/main.py
@@ -9,6 +9,7 @@ from step_crawler.functions_file import *
 from step_crawler import functions_file
 
 from pyppeteer import launch
+from pyext import RuntimeModule
 
 
 async def main():
@@ -17,7 +18,6 @@ async def main():
     await page.goto('http://cnes.datasus.gov.br/pages/estabelecimentos/'
                     'consulta.jsp')
 
-    steps = __import__('steps')
     await steps.execute_steps(page = page)
 
     await browser.close()
@@ -27,13 +27,12 @@ async def main():
 with open('recipe.json') as file:
     recipe = json.load(file)
 
-with open('steps.py', 'w+') as file:
-    # code = code_g.generate_code(recipe, functions_file)
-    code = code_g.generate_code(atom.extend(recipe)[0], functions_file)
-    print(code)
-    file.write(code)
+code = code_g.generate_code(atom.extend(recipe)[0], functions_file)
+steps = RuntimeModule.from_string("steps", code)
+print(code)
 
 asyncio.get_event_loop().run_until_complete(main())
+
 
 
 
