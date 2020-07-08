5990ac71b0ac28d7df2d997271d29d068e99344e
gabrielcrds <gabriel.d.a.cardoso@gmail.com>
2020-07-08 16:31:15 -0300

    Ignores OperationalError em running MainConfig.ready

--- main/apps.py
+++ main/apps.py
@@ -1,14 +1,28 @@
 from django.apps import AppConfig
-
+from django.db.utils import OperationalError
 
 class MainConfig(AppConfig):
     name = 'main'
+    server_running = False
 
-    def ready(self):
+    def runOnce(self):
+        if self.server_running:
+            return
+        self.server_running = True
         # have to import here, after everything is ready
         from .models import CrawlerInstance
         instances = CrawlerInstance.objects.filter(running=True)
-        
+
         for instance in instances:
             instance.running = False
             instance.save()
+
+    def ready(self):
+        try:
+            self.runOnce()
+        except OperationalError as err:
+            print(
+                f"Error at MainConfig.ready(). Message:\n{err}\n"
+                f"Are you making migrations or migrating?\n" 
+                f" If so, ignore this error. Otherwise you should fix it."
+            )
