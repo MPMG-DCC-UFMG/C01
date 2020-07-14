a31f7a48ee8362b9bbd2c50509475ebc3e835c7b
TalesPanoutsos <tales.panoutsos@gmail.com>
2020-07-14 00:55:53 -0300

    Creating new system of generation

--- step-by-step/step_crawler/code_generator.py
+++ step-by-step/step_crawler/code_generator.py
@@ -1,4 +1,6 @@
 import inspect
+import step_crawler.step_generators as step_generators
+
 
 
 def dict_to_arguments(dict_of_arguments):
@@ -43,80 +45,11 @@ def generate_body(recipe, module):
     """
     code = ""
     for child in recipe['children']:
-        if child['step'] == 'for':
-
-            if 'call' in child['iterable']:
-                is_coroutine = inspect.iscoroutinefunction(
-                    getattr(module, child['iterable']['call']['step']))
-                iterable = generate_call(child['iterable']['call']['step'],
-                                         child['iterable']['call']
-                                         ['arguments'],
-                                         is_coroutine)
-            elif 'object' in child['iterable']:
-                iterable = '[' + ', '.join(child['iterable']['object']) + ']'
-            else:
-                raise TypeError('This iterable is in the wrong format')
-
-            code += child['depth'] * '    ' \
-                + 'for ' + child['iterator'] \
-                + ' in '\
-                + iterable\
-                + ':' + '\n'
-            code += generate_body(child, module)
-
-        elif child['step'] == 'while':
-            is_coroutine = inspect.iscoroutinefunction(
-                getattr(module, child['condition']['step']))
-            call = generate_call(child['condition']['step'],
-                                 child['condition']['arguments'], is_coroutine)
-
-            code += child['depth'] * '    ' \
-                + 'while ' + call + ':' + '\n'
-            code += generate_body(child, module)
-
-        elif child['step'] == 'if':
-            pass
-
-        elif child['step'] == 'attribution':
-            code += child['depth'] * '    ' + \
-                child['target'] + ' = ' + str(child['source']) + '\n'
-        elif child['step'] == 'para_cada_pagina_em':
-            code += child['depth'] * '    '\
-                + 'clickable = True' + '\n'\
-                + child['depth'] * '    '\
-                + 'while clickable:' + '\n'\
-                + generate_body(child, module)\
-                + (1 + child['depth']) * '    '\
-                + "buttons = await page.xpath("+child["xpath_dos_botoes"]+")\n" \
-                + (1 + child['depth']) * '    '\
-                + "if len(buttons) !=0: \n" \
-                + (1 + child['depth']) * '    '\
-                + "    next_button = buttons["+ str(child["indice_do_botao_proximo"]) +"] \n" \
-                + (1 + child['depth']) * '    '\
-                + "    before_click = await page.content() \n" \
-                + (1 + child['depth']) * '    '\
-                + "    await next_button.click() \n" \
-                + (1 + child['depth']) * '    '\
-                + "    after_click = await page.content() \n" \
-                + (1 + child['depth']) * '    '\
-                + "    if before_click == after_click: \n" \
-                + (1 + child['depth']) * '    '\
-                + "        clickable = False \n" \
-                + (1 + child['depth']) * '    '\
-                + "else: \n" \
-                + (1 + child['depth']) * '    '\
-                + "    clickable = False \n" \
-
-        elif child['step'] == 'salva_pagina':
-            code += child['depth'] * '    ' + "pages[gera_nome_arquivo()] = " + \
-                    "await salva_pagina(**missing_arguments)\n"
-
+        if hasattr(step_generators, "generate_" + child["step"]):
+            code += getattr(step_generators, "generate_" + child["step"])(child, module)
         else:
-            is_coroutine = inspect.iscoroutinefunction(getattr(module,
-                                                               child['step']))
-            code += child['depth'] * '    ' \
-                + generate_call(child['step'], child['arguments'],
-                                is_coroutine) + '\n'
+            code += step_generators.generate_call_step(child, module)
+
     return code
 
 
