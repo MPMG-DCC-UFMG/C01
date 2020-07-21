041c0e5f5da8135704fb0a5f347051467db35418
Rennan Cordeiro <rennancl@gmail.com>
2020-07-21 15:06:09 -0300

    fixing setup

--- step-by-step/setup.py
+++ step-by-step/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='Step Crawler',
+    name='step_cralwer',
     version='1.0',
     description='Module creating step by step crawlers',
     long_description=long_description,
@@ -14,6 +14,5 @@ setup(
     author='Tales Panoutsos',
     author_email='TalesPanoutsos@users.noreply.github.com',
     packages=['step_crawler'],
-    install_requires=['inspect', 'cssify', 'selenium', 'asyncio', 'copy',
-                      'pyppeteer', 'pyext']
+    install_requires=['cssify', 'selenium', 'asyncio','pyppeteer2', 'pyext']
 )
