c06aec1409cc8670c4fe69fa9d9958f9d74b2c8d
gabrielcrds <gabriel.d.a.cardoso@gmail.com>
2020-08-18 17:25:15 -0300

    Adds installation scripts and instructions

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='C04',
+    version='0.1',
+    classifiers=["Programming Language :: Python :: 3"],
+    install_requires=['django', 'django-crispy-forms', 'scrapy', 'requests'],
+)
