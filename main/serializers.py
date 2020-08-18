aaadbb5ffda5f91fa42d0c122dc3e76f61fb6b32
PEP 8 Speaks <pep8speaks@gmail.com>
2020-08-19 04:21:43 +0530

    Fix PEP 8 errors in main/serializers.py

--- main/serializers.py
+++ main/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from .models import CrawlRequest, CrawlerInstance
 
+
 class CrawlerInstanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = CrawlerInstance
