99800a37e4fa4889bff39a000c65ccf758a20214
gabrielcrds <gabriel.d.a.cardoso@gmail.com>
2020-08-21 08:38:16 -0300

    Change the way of calling subprocess.run

--- install.py
+++ install.py
@@ -6,13 +6,13 @@ src_folder = "src"
 for folder in os.listdir(f"{src_folder}"):
     if "setup.py" in os.listdir(f"{src_folder}/{folder}"):
         print(f"Installing {folder}...")
-        subprocess.run(f"pip install {src_folder}/{folder}")
+        subprocess.run(["pip", "install", f"{src_folder}/{folder}"])
         print()
 
 print(f"Installing other project dependencies...")
-subprocess.run("pip install .")
+subprocess.run(["pip", "install", "."])
 print()
 
 print(f"Creating database...")
-subprocess.run("python manage.py migrate")
+subprocess.run(["python", "manage.py", "migrate"])
 print()
