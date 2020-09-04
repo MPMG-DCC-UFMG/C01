627e000e48dc312e371baeb4a1628017a831e335
gabrielcrds <gabriel.d.a.cardoso@gmail.com>
2020-09-04 14:18:47 -0300

    Change encoding from ascii to utf-8

--- crawlers/base_spider.py
+++ crawlers/base_spider.py
@@ -144,7 +144,7 @@ class BaseSpider(scrapy.Spider):
 
             file_mode = "w+"
             body = cleaner.clean_html(
-                response.body.decode('ascii', errors='ignore'))
+                response.body.decode('utf-8', errors='ignore'))
 
         hsh = crawling_utils.hash(response.url)
 
@@ -181,19 +181,16 @@ class BaseSpider(scrapy.Spider):
         # you may need the failure's type
         self.logger.error(repr(failure))
 
-        # if isinstance(failure.value, HttpError):
         if failure.check(HttpError):
             # you can get the response
             response = failure.value.response
             self.logger.error('HttpError on %s', response.url)
 
-        # elif isinstance(failure.value, DNSLookupError):
         elif failure.check(DNSLookupError):
             # this is the original request
             request = failure.request
             self.logger.error('DNSLookupError on %s', request.url)
 
-        # elif isinstance(failure.value, TimeoutError):
         elif failure.check(TimeoutError):
             request = failure.request
             self.logger.error('TimeoutError on %s', request.url)
