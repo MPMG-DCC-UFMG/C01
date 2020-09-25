5fecef5c68e8a7c604eaa1bf31763d105f9291ef
TalesPanoutsos <tales.panoutsos@gmail.com>
2020-09-25 10:00:25 -0300

    Correcting pep8

--- main/views.py
+++ main/views.py
@@ -37,7 +37,7 @@ def process_run_crawl(crawler_id):
 
         del data['creation_date']
         del data['last_modified']
-        
+
         if data["output_path"] is None:
             data["output_path"] = CURR_FOLDER_FROM_ROOT
         else:
@@ -54,7 +54,8 @@ def process_run_crawl(crawler_id):
 
 
 def process_stop_crawl(crawler_id):
-    instance = CrawlRequest.objects.filter(id=crawler_id).get().running_instance
+    instance = CrawlRequest.objects.filter(
+        id=crawler_id).get().running_instance
     # instance = CrawlerInstance.objects.get(instance_id=instance_id)
 
     # No instance running
@@ -81,7 +82,8 @@ def getAllData():
 
 def create_instance(crawler_id, instance_id):
     mother = CrawlRequest.objects.filter(id=crawler_id)
-    obj = CrawlerInstance.objects.create(crawler_id=mother[0], instance_id=instance_id, running=True)
+    obj = CrawlerInstance.objects.create(
+        crawler_id=mother[0], instance_id=instance_id, running=True)
     return obj
 
 # Views
@@ -107,7 +109,6 @@ def create_crawler(request):
     return render(request, "main/create_crawler.html", context)
 
 
-
 def edit_crawler(request, id):
     crawler = get_object_or_404(CrawlRequest, pk=id)
     form = RawCrawlRequestForm(request.POST or None, instance=crawler)
@@ -117,7 +118,11 @@ def edit_crawler(request, id):
             form.save()
             return redirect('list_crawlers')
     else:
-        return render(request, 'main/create_crawler.html', {'form': form, 'crawler': crawler})
+        return render(
+            request,
+            'main/create_crawler.html',
+            {'form': form, 'crawler': crawler}
+        )
 
 
 def delete_crawler(request, id):
@@ -127,13 +132,18 @@ def delete_crawler(request, id):
         crawler.delete()
         return redirect('list_crawlers')
 
-    return render(request, 'main/confirm_delete_modal.html', {'crawler': crawler})
+    return render(
+        request,
+        'main/confirm_delete_modal.html',
+        {'crawler': crawler}
+    )
 
 
 def detail_crawler(request, id):
     crawler = CrawlRequest.objects.get(id=id)
     # order_by("-atribute") orders descending
-    instances = CrawlerInstance.objects.filter(crawler_id=id).order_by("-last_modified")
+    instances = CrawlerInstance.objects.filter(
+        crawler_id=id).order_by("-last_modified")
 
     context = {
         'crawler': crawler,
@@ -162,18 +172,23 @@ def run_crawl(request, crawler_id):
     return redirect(detail_crawler, id=crawler_id)
 
 
-
-
 def tail_log_file(request, instance_id):
 
-
-    crawler_id = CrawlerInstance.objects.filter(instance_id=instance_id).values()[0]["crawler_id_id"]
+    crawler_id = CrawlerInstance.objects.filter(
+        instance_id=instance_id
+    ).values()[0]["crawler_id_id"]
 
     config = CrawlRequest.objects.filter(id=int(crawler_id)).values()[0]
     output_path = config["output_path"]
 
-    out = subprocess.run(["tail", f"{output_path}/log/{instance_id}.out", "-n", "10"], stdout=subprocess.PIPE).stdout
-    err = subprocess.run(["tail", f"{output_path}/log/{instance_id}.err", "-n", "10"], stdout=subprocess.PIPE).stdout
+    out = subprocess.run(
+        ["tail", f"{output_path}/log/{instance_id}.out", "-n", "10"],
+        stdout=subprocess.PIPE
+    ).stdout
+    err = subprocess.run(
+        ["tail", f"{output_path}/log/{instance_id}.err", "-n", "10"],
+        stdout=subprocess.PIPE
+    ).stdout
     return JsonResponse({
         "out": out.decode('utf-8'),
         "err": err.decode('utf-8'),
