2055d256cecc46b1ca88d8ca94e953089834ef5e
gabrielcrds <gabriel.d.a.cardoso@gmail.com>
2020-09-30 09:24:54 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setuptools.setup(
     classifiers=["Programming Language :: Python :: 3"],
     install_requires=[
         'django', 'django-crispy-forms', 'djangorestframework',
-        'scrapy', 'requests', 'pytest'
+        'scrapy', 'requests', 'pytest', 'pytest-mock', 'python3-wget'
     ],
 )
