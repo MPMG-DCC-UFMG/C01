97588af1aa30b83b8d8700bdc369fc0d809b89c0
Elves Rodrigues <elvesmateusrodrigues@outlook.com>
2020-10-01 13:56:50 -0300

    Adições de docstrings + geração e tratamento de exceções

--- src/scheduler_plugin/scheduler/scheduler.py
+++ src/scheduler_plugin/scheduler/scheduler.py
@@ -9,6 +9,7 @@ import time
 from datetime import datetime, timedelta
 from kafka import KafkaProducer
 
+
 class SchedulerPlugin(BaseHandler):
     schema = "scheduler_schema.json"
     scheduled = set()
@@ -50,7 +51,7 @@ class SchedulerPlugin(BaseHandler):
         '''
 
         self.schedule(dict)
-    
+
         dict['parsed'] = True
         dict['valid'] = True
 
@@ -96,16 +97,15 @@ class SchedulerPlugin(BaseHandler):
         Returns:
             Returns the time of the next crawl defined by `start_at`, or None, if something went wrong.
 
+        Raises: 
+            ValueError: Whether the time is in the past or if it is invalid.
+
         '''
 
-        try:
-            next_crawl = datetime.strptime(start_at, '%Y-%m-%d %H:%M')
-            if next_crawl < now:
-                return None
-            return next_crawl
-
-        except:
-            return None
+        next_crawl = datetime.strptime(start_at, '%Y-%m-%d %H:%M')
+        if next_crawl < now:
+            raise ValueError(f'\'{start_at}\' is a time in the past.')
+        return next_crawl
 
     def get_next_crawl_by_minutes(self, now: datetime, delta: int) -> datetime:
         '''Returns the time of the next crawl based on the `delta` step in minutes.
@@ -121,24 +121,24 @@ class SchedulerPlugin(BaseHandler):
 
         return now + timedelta(minutes=delta)
 
-    def get_next_crawl_by_hours(self, now: datetime, delta: int, at_minute: int = -1, from_hour: int = -1, to_hour: int = -1) -> datetime:
-        '''Returns the time of the next collection based on the `delta` step in hours.
+    def get_next_crawl_by_hours(self, now: datetime, delta: int, at_minute: int = None, from_hour: int = None, to_hour: int = None) -> datetime:
+        '''Returns the time of the next crawl based on the `delta` step in hours.
 
         Args:
             now: Current time.
             delta: Step size in hours for the next crawl.
-            at_minute: If greater than or equal to zero, the minute the crawl should be processed.
-            from_hour: If greater than or equal to zero and `to_hour` also, the shortest time the crawl must be processed.
-            to_hour: If greater than or equal to zero and `from_hour` as well, the longest time the crawl must be processed.
+            at_minute: If not None, the minute the crawl should be processed. Else, the current minute will be used.
+            from_hour: If not None and `to_hour` also, the shortest time the crawl must be processed.
+            to_hour: If not None and `from_hour` as well, the longest time the crawl must be processed.
 
         Returns:
-            Returns the next collection time.
+            Returns the next crawl time.
 
         '''
 
         next_crawl = now + timedelta(hours=delta)
 
-        if from_hour >= 0 and to_hour >= 0:
+        if from_hour is not None and to_hour is not None:
             crawl_hour = next_crawl.hour
 
             if from_hour > to_hour:
@@ -151,21 +151,21 @@ class SchedulerPlugin(BaseHandler):
                 hours_shift = self.calculate_hour_shift(crawl_hour, from_hour)
                 next_crawl += timedelta(hours=hours_shift)
 
-        if at_minute >= 0:
+        if at_minute is not None:
             return next_crawl.replace(minute=at_minute)
 
         return next_crawl
 
-    def get_next_crawl_by_days(self, now: datetime, delta: int, at_hour: int = -1, at_minute: int = -1, from_weekday: int = -1, to_weekday: int = -1) -> datetime:
-        '''Returns the next collection time based on the `delta` step in days.
+    def get_next_crawl_by_days(self, now: datetime, delta: int, at_hour: int = None, at_minute: int = None, from_weekday: int = None, to_weekday: int = None) -> datetime:
+        '''Returns the next crawl time based on the `delta` step in days.
 
         Args:
             now: Current time.
             delta: Step in days for the next crawl.
-            at_hour: If greater than or equal to 0, the time the crawl should be processed.
-            at_minute: If greater than or equal to 0, the minute the crawl must be processed.
-            from_weekday: If greater than or equal to 0 and `to_weekday` also, the minimum day of the week that the crawl must be processed. Where Monday = 0.. Sunday = 6
-            to_weekday: If greater than or equal to 0 and `from_weekday` also, the maximum day of the week that the crawl should be processed. Where Monday = 0.. Sunday = 6
+            at_hour: If not None, the time the crawl should be processed. Else, the current hour will be used. (default None)
+            at_minute: If not None, the minute the crawl must be processed. Else, the current minute will be used. (default None)
+            from_weekday: If not None and `to_weekday` also, the minimum day of the week that the crawl must be processed. Where Monday = 0.. Sunday = 6
+            to_weekday: If not None and `from_weekday` also, the maximum day of the week that the crawl should be processed. Where Monday = 0.. Sunday = 6
     
         Returns:
             Returns the next crawl time.
@@ -174,7 +174,7 @@ class SchedulerPlugin(BaseHandler):
 
         next_crawl = now + timedelta(days=delta)
 
-        if from_weekday >= 0 and to_weekday >= 0:
+        if from_weekday is not None and to_weekday is not None:
             crawl_weekday = next_crawl.weekday()
 
             if from_weekday > to_weekday:
@@ -188,32 +188,32 @@ class SchedulerPlugin(BaseHandler):
                     crawl_weekday, from_weekday)
                 next_crawl += timedelta(days=days_shift)
 
-        if at_hour >= 0:
+        if at_hour is not None:
             next_crawl = next_crawl.replace(hour=at_hour)
 
-        if at_minute >= 0:
+        if at_minute is not None:
             next_crawl = next_crawl.replace(minute=at_minute)
 
         return next_crawl
 
-    def get_next_crawl_by_weeks(self, now: datetime, delta: int, at_weekday: int = -1, at_hour: int = -1, at_minute: int = -1) -> datetime:
+    def get_next_crawl_by_weeks(self, now: datetime, delta: int, at_weekday: int = None, at_hour: int = None, at_minute: int = None) -> datetime:
         '''Returns the next crawl time based on the `delta` step in weeks.
 
         Args:
             now: Current time.
             delta: Step size in weeks for the next crawl.
-            at_weekday: If greater than or equal to 0, day of the week that the scheduled crawl should be processed. Where Monday = 0 ... Sunday = 6
-            at_hour: If greater than or equal to 0, time that the scheduled crawl must be processed.
-            at_minute: If greater than or equal to 0, minute that the scheduled crawl should be processed.
+            at_weekday: Day of the week that the scheduled crawl should be processed. Where Monday = 0 ... Sunday = 6. If None, the current weekday hour will be used.
+            at_hour: Hour that the scheduled crawl must be processed. If None, the current hour will be used. 
+            at_minute: Minute that the scheduled crawl should be processed. If None, the current minute will be used.
     
         Returns:
-            Returns the time of the next collection.
+            Returns the time of the next crawl.
 
         '''
 
         next_crawl = now + timedelta(weeks=delta)
 
-        if at_weekday >= 0:
+        if at_weekday is not None:
             crawl_weekday = next_crawl.weekday()
 
             if crawl_weekday != at_weekday:
@@ -221,42 +221,45 @@ class SchedulerPlugin(BaseHandler):
                     crawl_weekday, at_weekday)
                 next_crawl += timedelta(days=days_shift)
 
-        if at_hour >= 0:
+        if at_hour is not None:
             next_crawl = next_crawl.replace(hour=at_hour)
 
-        if at_minute >= 0:
+        if at_minute is not None:
             next_crawl = next_crawl.replace(minute=at_minute)
 
         return next_crawl
 
     def validate_field(self, conf: dict, key: str, min_val: int, max_val: int) -> int:
-        '''Validates that the value of `key` in` conf` respects the restrictions of `min_val` and` max_val`
+        '''Validates that the value of key in conf respects the restrictions of min_val and max_val
 
         Args:
             conf: Dictionary with crawl settings.
-            key: Key to a value in `conf` that will be validated.
-            min_val: Minimum value that the value of the key in `conf` is valid.
-            max_val: Maximum value that the value of the key in `conf` is valid.
+            key: Key to a value in conf that will be validated.
+            min_val: Minimum value that the value of the key in conf is valid.
+            max_val: Maximum value that the value of the key in conf is valid.
     
         Returns:
-            The value of `key` in` conf` or -1 if the restrictions have not been satisfied.
+            The value of key in conf or None, if the field is not present.
 
+        Raises:
+            TypeError: If the value of field key in conf is not an integer.
+            ValueError: If the value of field key in conf is not between min_val and max_val.
         '''
+        if key not in conf:
+            return None 
 
-        if key in conf:
-            val = conf[key]
+        val = conf[key]
 
-            if type(val) is not int:
-                return -1
+        if type(val) is not int:
+            raise TypeError(
+                f'The value in field \'{key}\' must be an integer.')
 
-            elif val < min_val or val > max_val:
-                return -1
-
-            else:
-                return val
+        elif val < min_val or val > max_val:
+            raise ValueError(
+                f'\'{val}\' is a invalid value to field \'{key}\'')
 
         else:
-            return -1
+            return val
 
     def parse_hour_conf(self, conf: dict) -> tuple:
         '''Validates and/or standardizes the crawls settings fields per hour.
@@ -274,9 +277,10 @@ class SchedulerPlugin(BaseHandler):
         from_hour = self.validate_field(conf, 'from', 0, 23)
         to_hour = self.validate_field(conf, 'to', 0, 23)
 
-        if from_hour == -1 or to_hour == -1:
-            from_hour = -1
-            to_hour = -1
+        # It is necessary to have a beginning and an end
+        if from_hour is None or to_hour is None:
+            from_hour = None
+            to_hour = None
 
         return at_minute, from_hour, to_hour
 
@@ -297,9 +301,10 @@ class SchedulerPlugin(BaseHandler):
         from_weekday = self.validate_field(conf, 'from', 0, 6)
         to_weekday = self.validate_field(conf, 'to', 0, 6)
 
-        if from_weekday == -1 or to_weekday == -1:
-            from_weekday = -1
-            to_weekday = -1
+        # It is necessary to have a beginning and an end
+        if from_weekday is None or to_weekday is None:
+            from_weekday = None
+            to_weekday = None
 
         return at_hour, at_minute, from_weekday, to_weekday
 
@@ -329,55 +334,58 @@ class SchedulerPlugin(BaseHandler):
         
         Returns:
             Returns the time of the next crawl or None, if it has been configured incorrectly.
-        
+
+        Raises:
+            TypeError: If the value of field 'every' in conf is not an integer.
+            ValueError: If the value of field 'every' in conf is less than or equal to 0, if the crawl
+                interval is not between minutes, hours, days and weeks, or if no scheduling settings are in conf.
         '''
 
-        try:
-            if conf.get('start_at'):
-                next_crawl = self.get_next_crawl_by_time(now, conf['start_at'])
-                del conf['start_at']
-                return next_crawl
+        if conf.get('start_at'):
+            next_crawl = self.get_next_crawl_by_time(now, conf['start_at'])
+            del conf['start_at']
+            return next_crawl
 
-            elif conf.get('repeat'):
-                conf = conf['repeat']
+        elif conf.get('repeat'):
+            conf = conf['repeat']
 
-                delta = conf['every']
-                if type(delta) is not int:
-                    return None
+            delta = conf['every']
+            if type(delta) is not int:
+                raise TypeError(
+                    'The step size between one crawl and another must be an integer.')
 
-                if delta < 1:
-                    return None
+            if delta < 1:
+                raise ValueError(
+                    'The step size between crawls must be greater than 0')
 
-                interval = conf['interval']
+            interval = conf['interval']
 
-                if interval == 'minutes':
-                    return self.get_next_crawl_by_minutes(now, delta)
+            if interval == 'minutes':
+                return self.get_next_crawl_by_minutes(now, delta)
 
-                elif interval == 'hours':
-                    at_minute, from_hour, to_hour = self.parse_hour_conf(conf)
-                    return self.get_next_crawl_by_hours(now, delta, at_minute, from_hour, to_hour)
+            elif interval == 'hours':
+                at_minute, from_hour, to_hour = self.parse_hour_conf(conf)
+                return self.get_next_crawl_by_hours(now, delta, at_minute, from_hour, to_hour)
 
-                elif interval == 'days':
-                    at_hour, at_minute, from_weekday, to_weekday = self.parse_day_conf(
-                        conf)
-                    return self.get_next_crawl_by_days(now, delta, at_hour, at_minute, from_weekday, to_weekday)
+            elif interval == 'days':
+                at_hour, at_minute, from_weekday, to_weekday = self.parse_day_conf(
+                    conf)
+                return self.get_next_crawl_by_days(now, delta, at_hour, at_minute, from_weekday, to_weekday)
 
-                elif interval == 'weeks':
-                    at_weekday, at_hour, at_minute = self.parse_week_conf(conf)
-                    return self.get_next_crawl_by_weeks(now, delta, at_weekday, at_hour, at_minute)
-
-                else:
-                    return None
+            elif interval == 'weeks':
+                at_weekday, at_hour, at_minute = self.parse_week_conf(conf)
+                return self.get_next_crawl_by_weeks(now, delta, at_weekday, at_hour, at_minute)
 
             else:
-                return None
-        
-        except Exception as e:
-            self.logger.error(str(e))
-            return None
+                raise ValueError(
+                    'Intervals between crawls must be: minutes, hours, days or weeks')
+
+        else:
+            raise ValueError(
+                'It is necessary to define when a crawl should be made and/or its repetition configuration.')
 
     def schedule_crawl(self, timestamp: str, crawl: dict) -> bool:
-        '''Schedule a visit.
+        '''Schedule a crawl.
 
         Args:
             timestamp: Timestamp of when crawl will occur.
@@ -414,14 +422,11 @@ class SchedulerPlugin(BaseHandler):
 
         return crawls
 
-    def send_crawl(self, crawl: dict) -> None:
+    def send_crawl(self, crawl: dict):
         '''Sends a crawl to be processed by the Scrapy Cluster.
         
         Args:
             crawl: Dictionary containing information about the visit to be performed..
-        
-        Returns:
-            None.
 
         '''
 
@@ -431,8 +436,8 @@ class SchedulerPlugin(BaseHandler):
         self.producer.send(self.incoming_topic, crawl)
         self.logger.info('Crawl sent to Kafka by Scheduler')
 
-    def schedule(self, req: dict) -> bool:
-        '''Schedule a visit.
+    def schedule(self, req: dict):
+        '''Interface to schedule a crawl.
 
         Args:
             req: Request for crawl schedule.
@@ -444,30 +449,35 @@ class SchedulerPlugin(BaseHandler):
 
         # removes metadata from the SC, as well as null default values assigned by it.
         req = dict((key, req[key]) for key in req if req[key])
-        
+
         if 'scheduler' not in req:
-            return False 
-        
+            return
+
         crawl_req = req['scheduler']
-        now = datetime.now()
-        next_crawl_time = self.get_next_crawl_time(now, crawl_req)
 
-        if next_crawl_time:
+        try:
+            now = datetime.now()
+            next_crawl_time = self.get_next_crawl_time(now, crawl_req)
             timestamp = next_crawl_time.strftime("%Y-%m-%d %H:%M")
+
             if self.schedule_crawl(timestamp, req):
-                self.logger.info(
-                    f'Crawl scheduled for {timestamp} sucessfully')
-                return 
+                self.logger.info(f'Crawl scheduled for {timestamp} sucessfully')
+            
+            else:
+                self.logger.warning(
+                    f'It was not possible to schedule the crawl, check the connection with Redis.')
 
-        self.logger.info("Failed to schedule crawl")
+        except Exception as e:
+            self.logger.error(e)
+            self.logger.info(f'Failed to schedule crawl')
 
-    def daemon(self) -> None:
+    def daemon(self):
         '''Thread that checks if it is time for a scheduled crawl is in time to be processed by the Scrapy Cluster and to be rescheduled.
         '''
 
         while True:
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
-            
+
             if timestamp in self.scheduled:
                 scheduled_crawls = self.get_scheduled_crawls(timestamp)
 
@@ -479,7 +489,8 @@ class SchedulerPlugin(BaseHandler):
 
                 self.producer.flush()
                 self.scheduled.remove(timestamp)
-
+            
+            #Wait for the new crawl interval
             time.sleep(60 - datetime.now().second)
 
     def run_daemon(self):
--- tests/.scheduler_plugin/test_scheduler.py
+++ tests/.scheduler_plugin/test_scheduler.py
@@ -4,7 +4,7 @@ import ujson
 import time
 import threading
 
-from m_scheduler import SchedulerPlugin
+from tests.m_scheduler import SchedulerPlugin
 from datetime import datetime, timedelta
 
 from kafka import KafkaConsumer
@@ -19,6 +19,7 @@ SETTINGS = {
     'KAFKA_INCOMING_TOPIC': 'demo.incoming'
 }
 
+
 class TestScheduler(unittest.TestCase):
     def setUp(self):
         self.crawls_timestamp_received = None
@@ -29,7 +30,7 @@ class TestScheduler(unittest.TestCase):
     def test_weekday_shift(self):
         '''Verifica se o cálculo de intervalos entre dias está correto.
         '''
-        
+
         scheduler = SchedulerPlugin()
 
         for from_weekday in range(7):
@@ -42,8 +43,9 @@ class TestScheduler(unittest.TestCase):
                     if curr_weekday == 7:
                         curr_weekday = 0
                     num_days += 1
-                
-                self.assertTrue(num_days == scheduler.calculate_weekday_shift(from_weekday, to_weekday))
+
+                self.assertTrue(num_days == scheduler.calculate_weekday_shift(
+                    from_weekday, to_weekday))
 
     def test_hour_shift(self):
         '''Verifica se o cálculo de intervalo entre horas está correto.
@@ -62,20 +64,21 @@ class TestScheduler(unittest.TestCase):
                         curr_hour = 0
                     num_hours += 1
 
-                self.assertTrue(num_hours == scheduler.calculate_hour_shift(from_hour, to_hour))
+                self.assertTrue(
+                    num_hours == scheduler.calculate_hour_shift(from_hour, to_hour))
 
     def test_get_next_crawl_by_time(self):
         '''Verifica se é possível agendar um horário específico para que uma coleta seja realizada ou comece.
         '''
-        
+
         scheduler = SchedulerPlugin()
 
-        # Tenta criar timestamp para uma coleta no passado 
-        now = datetime.now()
-        conf = {'start_at': '2020-07-12 23:59'}
-
-        next_crawl = scheduler.get_next_crawl_time(now, conf)
-        self.assertTrue(next_crawl is None)
+        # Tenta criar timestamp para uma coleta no passado
+        with self.assertRaises(ValueError):
+            now = datetime.now()
+            conf = {'start_at': '2020-07-12 23:59'}
+            next_crawl = scheduler.get_next_crawl_time(now, conf)
+            self.assertTrue(next_crawl is None)
 
         # Tenta criar timestamp de uma coleta no futuro
         now_delta = now + timedelta(hours=2)
@@ -86,27 +89,29 @@ class TestScheduler(unittest.TestCase):
         self.assertTrue(next_crawl is not None)
 
         # Tenta criar timestamp de coleta num horário inválido
-        conf = {'start_at': '2020-07-12 23:90'}
-        next_crawl = scheduler.get_next_crawl_time(now, conf)
-        self.assertTrue(next_crawl is None)
+        with self.assertRaises(ValueError):
+            conf = {'start_at': '2020-07-12 23:90'}
+            next_crawl = scheduler.get_next_crawl_time(now, conf)
+            self.assertTrue(next_crawl is None)
 
         # Tenta criar um timestamp de coleta num formato inválido
-        conf = {'start_at': '2020-07-12, 23:90'}
-        next_crawl = scheduler.get_next_crawl_time(now, conf)
-        self.assertTrue(next_crawl is None)
+        with self.assertRaises(ValueError):
+            conf = {'start_at': '2020-07-12, 23:90'}
+            next_crawl = scheduler.get_next_crawl_time(now, conf)
+            self.assertTrue(next_crawl is None)
 
     def test_get_next_crawl_by_minutes(self):
         '''Verifica agendamentos por minutos.
         '''
-        
+
         scheduler = SchedulerPlugin()
-        
+
         now = datetime.now()
         curr_minute = now.minute
 
         for delta_minute in range(1, 61):
             crawl_minute = (curr_minute + delta_minute) % 60
-            conf = {'repeat': {'every': delta_minute,'interval': 'minutes'}}
+            conf = {'repeat': {'every': delta_minute, 'interval': 'minutes'}}
             next_crawl = scheduler.get_next_crawl_time(now, conf)
             self.assertTrue(crawl_minute == next_crawl.minute)
 
@@ -135,33 +140,34 @@ class TestScheduler(unittest.TestCase):
                         }
 
                         crawl_hour = (curr_hour + delta_hour) % 24
-                        
+
                         if from_hour > to_hour:
                             if crawl_hour > to_hour and crawl_hour < from_hour:
                                 crawl_hour = from_hour
-                    
+
                         elif crawl_hour < from_hour or crawl_hour > to_hour:
                             crawl_hour = from_hour
-                            
+
                         next_crawl = scheduler.get_next_crawl_time(now, conf)
 
                         self.assertTrue(next_crawl.hour == crawl_hour)
                         self.assertTrue(next_crawl.minute == at_minute)
 
         # testa período de recoleta inválido
-        conf = {'repeat': {'every': 0, 'interval': 'minutes'}}
-        new_crawl = scheduler.get_next_crawl_time(now, conf)
-        self.assertTrue(new_crawl is None)
+        with self.assertRaises(ValueError):
+            conf = {'repeat': {'every': 0, 'interval': 'minutes'}}
+            new_crawl = scheduler.get_next_crawl_time(now, conf)
 
         # testa período inválido de recoleta
-        conf = {'repeat': {'every': -1, 'interval': 'minutes'}}
-        new_crawl = scheduler.get_next_crawl_time(now, conf)
-        self.assertTrue(new_crawl is None)
+        with self.assertRaises(ValueError):
+            conf = {'repeat': {'every': -1, 'interval': 'minutes'}}
+            new_crawl = scheduler.get_next_crawl_time(now, conf)
+            self.assertTrue(new_crawl is None)
 
     def test_get_next_crawl_by_days(self):
         '''Verifica todos parâmetros para agendamentos por dias.
         '''
-        
+
         scheduler = SchedulerPlugin()
         max_days = 8
 
@@ -189,30 +195,32 @@ class TestScheduler(unittest.TestCase):
                             if from_weekday > to_weekday:
                                 if crawl_weekday > to_weekday and crawl_weekday < from_weekday:
                                     crawl_weekday = from_weekday
-                            
+
                             elif crawl_weekday < from_weekday or crawl_weekday > to_weekday:
                                 crawl_weekday = from_weekday
-                            
-                            next_crawl = scheduler.get_next_crawl_time(now, conf)
-                            
-                            self.assertTrue(next_crawl.weekday() == crawl_weekday)  
+
+                            next_crawl = scheduler.get_next_crawl_time(
+                                now, conf)
+
+                            self.assertTrue(
+                                next_crawl.weekday() == crawl_weekday)
                             self.assertTrue(next_crawl.hour == at_hour)
                             self.assertTrue(next_crawl.minute == at_minute)
 
         # testa período de recoleta inválido
-        conf = {'repeat': {'every': 0, 'interval': 'days'}}
-        new_crawl = scheduler.get_next_crawl_time(now, conf)
-        self.assertTrue(new_crawl is None)
+        with self.assertRaises(ValueError):
+            conf = {'repeat': {'every': 0, 'interval': 'days'}}
+            next_crawl = scheduler.get_next_crawl_time(now, conf)
 
         # testa período inválido de recoleta
-        conf = {'repeat': {'every': -1, 'interval': 'days'}}
-        new_crawl = scheduler.get_next_crawl_time(now, conf)
-        self.assertTrue(new_crawl is None)
+        with self.assertRaises(ValueError):
+            conf = {'repeat': {'every': -1, 'interval': 'days'}}
+            new_crawl = scheduler.get_next_crawl_time(now, conf)
 
     def test_get_next_crawl_by_weeks(self):
         '''Verifica todos parâmetros para agendamentos por semanas.
         '''
-        
+
         scheduler = SchedulerPlugin()
         max_weeks = 52
 
@@ -238,19 +246,19 @@ class TestScheduler(unittest.TestCase):
                         self.assertTrue(new_crawl.minute == at_minute)
 
         # testa período de recoleta inválido
-        conf = {'repeat': {'every': 0,'interval': 'weeks'}}
-        new_crawl = scheduler.get_next_crawl_time(now, conf)
-        self.assertTrue(new_crawl == None)
+        with self.assertRaises(ValueError):
+            conf = {'repeat': {'every': 0, 'interval': 'weeks'}}
+            new_crawl = scheduler.get_next_crawl_time(now, conf)
 
         # testa período inválido de recoleta
-        conf = {'repeat': {'every': -1,'interval': 'weeks',}}
-        new_crawl = scheduler.get_next_crawl_time(now, conf)
-        self.assertTrue(new_crawl == None)
+        with self.assertRaises(ValueError):
+            conf = {'repeat': {'every': -1, 'interval': 'weeks', }}
+            new_crawl = scheduler.get_next_crawl_time(now, conf)
 
     def _income_listener(self):
         '''Método com consumidor kafka que recebe mensagens do tópico de entrada do Scrapy Cluster.
         '''
-        
+
         self.crawls_timestamp_received = list()
         for message in self.kafka_consumer:
             timestamp = ujson.loads(message.value)['ts']
@@ -276,7 +284,7 @@ class TestScheduler(unittest.TestCase):
                 },
             }
         }
-        
+
         thread = threading.Thread(target=self._income_listener, daemon=True)
         thread.start()
 
@@ -285,6 +293,5 @@ class TestScheduler(unittest.TestCase):
         self.keep_running = False
 
         # Como recoletas ocorrem no intervalo de um minuto e o tempo esta passando virtualmente 60 segundos por segundo real,
-        # é esperado que haja, pelo menos, 60 requisições de coletas enviadas ao tópico de entrada do SC 
+        # é esperado que haja, pelo menos, 60 requisições de coletas enviadas ao tópico de entrada do SC
         self.assertTrue(len(self.crawls_timestamp_received) >= 60)
-        
