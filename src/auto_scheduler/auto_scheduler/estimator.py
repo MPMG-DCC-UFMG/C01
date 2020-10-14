b09bac7d3b39dd4d1caea54663edabc4190ea2b4
PEP 8 Speaks <pep8speaks@gmail.com>
2020-10-14 21:40:14 +0530

    Fix PEP 8 errors in src/auto_scheduler/auto_scheduler/estimator.py

--- src/auto_scheduler/auto_scheduler/estimator.py
+++ src/auto_scheduler/auto_scheduler/estimator.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+
 class Estimator:
     '''Implement some regular visit estimators. More details: https://oak.cs.ucla.edu/~cho/papers/cho-thesis.pdf
     '''
-    
+
     @staticmethod
     def estimate_by_changes(num_changes: int, num_visits: int, crawl_interval: float) -> float:
         ''' Estimates the frequency of changes in seconds based on the average changes detected in visits
@@ -12,12 +13,12 @@ class Estimator:
             num_changes: Number of changes detected in crawls
             num_visits: Number of visits made
             crawl_interval: Interval between crawls in seconds previously used
-        
+
         Returns:
             Returns the estimated frequency of changes in seconds
 
         '''
-        
+
         r = num_changes / num_visits
         return crawl_interval / r
 
@@ -29,10 +30,10 @@ class Estimator:
             num_changes: Number of changes detected in crawls
             num_visits: Number of visits made
             crawl_interval: Interval between crawls in seconds previously used
-        
+
         Returns:
             Returns the estimated frequency of changes in seconds
-            
+
         '''
 
         r = - np.log(((num_visits - num_changes) + .5) / (num_visits + .5))
