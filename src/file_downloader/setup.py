2645c7246c2bc48fd3cad61eec01bc2b2a886f49
LorenaMendes <lorenamendes.contato@gmail.com>
2020-11-10 22:15:07 -0300

    Created a module for Base Messenger

---
+++ src/file_downloader/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FileDownloader',
+    version='1.0',
+    description="Module for downloading crawled files",
+    license="MIT",
+    author='Gabriel Cardoso',
+    author_email='gabrielcrds@users.noreply.github.com',
+    packages=[],
+    install_requires=[]
+)
