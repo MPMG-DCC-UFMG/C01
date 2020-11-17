f8395887bf7896354420d1f27eeee1c446c1f695
LorenaMendes <lorenamendes.contato@gmail.com>
2020-11-17 16:51:23 -0300

    Added module for base messenger

---
+++ src/base_messenger/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='BaseMessenger',
+    version='1.0',
+    description="Module that provides base functions to send and receive messages from Kafka",
+    license="MIT",
+    author='Gabriel Cardoso',
+    author_email='gabrielcrds@users.noreply.github.com',
+    packages=setuptools.find_packages(),
+)
