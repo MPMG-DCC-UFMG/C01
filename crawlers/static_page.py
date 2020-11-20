e7b7154f3a1cf0c64f237755587ed69db7603f80
PEP 8 Speaks <pep8speaks@gmail.com>
2020-11-20 23:50:22 +0530

    Fix PEP 8 errors in crawlers/static_page.py

--- crawlers/static_page.py
+++ crawlers/static_page.py
@@ -68,7 +68,8 @@ class StaticPageSpider(BaseSpider):
     def filter_type_of_urls(self, url_list, page_flag):
         """Filter a list of urls according to the Content-Type."""
         def allow(url):
-            headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'}
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36'}
             req_head = requests.head(url, allow_redirects=True, headers=headers).headers['Content-Type']
             if (('html' in req_head) and page_flag) or (('html' not in req_head) and not page_flag):
                 # print(f"ADDING link (correct type) - {url}")
