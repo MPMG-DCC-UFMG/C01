f5c8d7193b31d7d6e32d0995dc3678649c39455c
PEP 8 Speaks <pep8speaks@gmail.com>
2020-12-08 21:01:16 +0530

    Fix PEP 8 errors in main/views.py

--- main/views.py
+++ main/views.py
@@ -8,7 +8,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 
 from .forms import CrawlRequestForm, RawCrawlRequestForm,\
-                   ResponseHandlerFormSet, ParameterHandlerFormSet
+    ResponseHandlerFormSet, ParameterHandlerFormSet
 from .models import CrawlRequest, CrawlerInstance
 from .serializers import CrawlRequestSerializer, CrawlerInstanceSerializer
 
