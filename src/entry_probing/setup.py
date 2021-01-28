bdabd606e7f2df90da50786b206572afb1278e66
Rennan Cordeiro <rennancl@gmail.com>
2021-01-28 15:59:05 -0300

    updating dependencies

--- src/entry_probing/setup.py
+++ src/entry_probing/setup.py
@@ -16,5 +16,5 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3"
     ],
-    install_requires=['requests', 'pyppeteer'],
+    install_requires=['requests', 'pyppeteer2'],
 )
