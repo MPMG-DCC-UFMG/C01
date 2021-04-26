9c79d8ec44b12a44908623c22f8bb1dcef54cfc0
Rennan Cordeiro <rennancl@gmail.com>
2021-04-26 09:45:16 -0300

    minor updates

--- install.py
+++ install.py
@@ -33,7 +33,7 @@ src_folder = "src"
 for folder in os.listdir(f"{src_folder}"):
     if "setup.py" in os.listdir(f"{src_folder}/{folder}"):
         print(f"Installing {folder}...")
-        subprocess.run(["pip", "--no-cache-dir","install", "--force-reinstall", f"{src_folder}/{folder}"])
+        subprocess.run(["pip", "install", f"{src_folder}/{folder}"])
         print()
 
 print(f"Installing other project dependencies...")
