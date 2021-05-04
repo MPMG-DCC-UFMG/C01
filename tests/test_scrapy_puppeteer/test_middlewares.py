32b8d421af0503ceb1de48aef4aec38dccbed5ff
Lucas Augusto <luc.aug.freire@gmail.com>
2021-05-04 16:23:32 -0300

    Atualiza testes de probing para usar mock correto
    Passa a utilizar o mock.create_autospec no lugar do Mock.MagicMock para
    capturar mais erros em potencial.

--- tests/test_scrapy_puppeteer/test_middlewares.py
+++ tests/test_scrapy_puppeteer/test_middlewares.py
@@ -1,29 +1,30 @@
-import asyncio
-from twisted.internet import asyncioreactor
+import os
 import scrapy
-from scrapy.crawler import CrawlerRunner, CrawlerProcess
-from twisted.internet import defer
+from scrapy.crawler import CrawlerProcess
 from twisted.trial.unittest import TestCase
 
+import unittest
 import scrapy_puppeteer
 
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 
-class ScrapyPuppeteerTestCase(TestCase):
+class ScrapyPuppeteerTestCase(unittest.TestCase):
     """Test case for the ``scrapy-puppeteer`` package"""
 
     class PuppeteerSpider(scrapy.Spider):
         name = 'puppeteer_crawl_spider'
         allowed_domains = ['ufmg.br']
+        handle_httpstatus_list = [0]
         items = []
 
         def start_requests(self):
-            yield scrapy_puppeteer.PuppeteerRequest(
-                'https://ufmg.br',
+            yield scrapy.Request(
+                "file://" + os.path.join(THIS_DIR, "test_files", "test1.html"),
                 wait_until='networkidle2'
             )
 
         def parse(self, response):
-            for selector_item in response.selector.xpath('//*[@id="rodape"]/section[1]/div/div[1]/div/ol/li'):
+            for selector_item in response.selector.xpath('//*[@id="lista-itens"]/li'):
                 self.items.append(selector_item)
 
     def setUp(self):
@@ -39,4 +40,4 @@ class ScrapyPuppeteerTestCase(TestCase):
         crawler = self.process.create_crawler(self.PuppeteerSpider)
         self.process.crawl(crawler)
         self.process.start()
-        self.assertEqual(len(crawler.spider.items), 12)
+        self.assertEqual(len(crawler.spider.items), 10)
