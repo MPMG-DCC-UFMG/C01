f6761da6967880329cf8f7f6fb614677a0d3fe58
PEP 8 Speaks <pep8speaks@gmail.com>
2021-05-19 04:26:01 +0530

    Fix PEP 8 errors in interface/settings.py

--- interface/settings.py
+++ interface/settings.py
@@ -170,8 +170,8 @@ if env('LOG_TO_FILE'):
         'disable_existing_loggers': True,
         'formatters': {
             'standard': {
-                'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-                'datefmt' : "%d/%b/%Y %H:%M:%S"
+                'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+                'datefmt': "%d/%b/%Y %H:%M:%S"
             },
         },
         'handlers': {
