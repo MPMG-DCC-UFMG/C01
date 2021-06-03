e41e35e7fb4284792918b842ef17f6b2e64c0eb1
Rennan Cordeiro <rennancl@gmail.com>
2021-06-03 09:57:45 -0300

    minor updates

--- docker-web/temp_run.sh
+++ docker-web/temp_run.sh
@@ -3,6 +3,6 @@
 python manage.py makemigrations --check --noinput;
 python3 manage.py migrate --noinput
 
-# # TODO Remove this file when FileDescriptor is dockerized
+# TODO Remove this file when FileDescriptor is dockerized
 gunicorn interface.wsgi:application -c gunicorn.conf.py &
 python3 temp_file_consumer.py
