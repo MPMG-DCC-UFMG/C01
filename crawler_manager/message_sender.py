2a4fc7815e6cd26896f74c2d63cf956cd9d14ea5
Elves Rodrigues <elvesmateusrodrigues@outlook.com>
2021-06-08 11:45:13 -0300

    Programmable interruption of link generation

--- crawler_manager/message_sender.py
+++ crawler_manager/message_sender.py
@@ -26,7 +26,7 @@ class MessageSender:
             'parameter_handlers': config['parameter_handlers']
         }
 
-        self.__producer.send(settings.LINK_GENERATOR_TOPIC, lite_config)
+        self.__producer.send(settings.LINK_GENERATOR_TOPIC, {'start': lite_config})
         self.__producer.send(settings.COMMANDS_TOPIC, {'create': config})
         self.__producer.flush()
 
@@ -36,5 +36,6 @@ class MessageSender:
         Args:
             - crawler_id: Unique crawler identifier
         """
+        self.__producer.send(settings.LINK_GENERATOR_TOPIC, {'stop': crawler_id})
         self.__producer.send(settings.COMMANDS_TOPIC, {'stop': crawler_id})
         self.__producer.flush()
--- link_generator/src/main.py
+++ link_generator/src/main.py
@@ -10,14 +10,32 @@ def run() -> None:
                             bootstrap_servers=settings.KAFKA_HOSTS,
                             value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
+    processes = dict()
+
     print('Waiting message...')
     for message in consumer:
         print('New Message received')
-        config = message.value
-        print('\tStarting a new process...')
-        p = Process(target=generate_requests, args=(config,))
-        p.start() 
-        print('\tProcess started')
+        message = message.value
+
+        if 'start' in message:
+            config = message['start']
+            crawler_id = str(config['crawler_id'])
+
+            print('\tStarting a new process...')
+            processes[crawler_id] = Process(target=generate_requests, args=(config,))
+            processes[crawler_id].start() 
+            print('\tProcess started')
+
+        elif 'stop' in message:
+            crawler_id = str(message['stop'])
+
+            if crawler_id in processes and processes[crawler_id].is_alive():
+                processes[crawler_id].terminate()
+
+            print(f'Generation of URLs for "{crawler_id}" stopped')
         
+        else:
+            pass 
+
 if __name__ == '__main__':
     run()
