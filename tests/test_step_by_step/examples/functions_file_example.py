5ff270a6cfaa3f078c4c7ce83d3e618220ec4e44
Rennan Cordeiro <rennancl@gmail.com>
2021-06-22 13:31:37 -0300

    fix tests

--- tests/test_step_by_step/examples/functions_file_example.py
+++ tests/test_step_by_step/examples/functions_file_example.py
@@ -1,17 +1,23 @@
+from step_crawler.functions_file import *
+
+@step
 def mandatory_and_optional_params(some, parameters, to=1, test=None):
     if test is None:
         test = {}
     return some, parameters, to, test
 
 
+@step
 def only_mandatory_params(some, parameters, to, test):
     return some, parameters, to, test
 
 
+@step
 def only_optional_params(some="some", parameters="parameters", to="to",
                          test="test"):
     return some, parameters, to, test
 
 
+@step
 def no_params():
     return [1, 2]
