ae76028986d8bf269b8c21359359e4b403ff7ddd
Lucas Augusto <luc.aug.freire@gmail.com>
2021-07-21 15:20:33 -0300

    Altera criação de pasta ao rodar o File Descriptor

--- broker_interface/start_descriptor.py
+++ broker_interface/start_descriptor.py
@@ -11,18 +11,11 @@ from broker_interface.file_description_consumer import FileDescriptionConsumer
 KAFKA_HOST = os.environ.get('KAFKA_HOST', 'localhost')
 KAFKA_PORT = os.environ.get('KAFKA_PORT', '9092')
 
-def check_file_path(path):
-    """Makes sure that folders in path exist."""
-    try:
-        os.makedirs(path)
-    except FileExistsError:
-        pass
-
 def start_consumer_process():
     """
     Redirects descriptor output and starts descriptor consumer loop.
     """
-    check_file_path("broker_interface/log/")
+    os.makedirs("broker_interface/log/", exist_ok=True)
     sys.stdout = open(f"broker_interface/log/file_descriptor.out", "w+",
        buffering=1)
     sys.stderr = open(f"broker_interface/log/file_descriptor.err", "w+",
