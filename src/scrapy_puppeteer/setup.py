34481d8f36de26e15fdd85ab237ce2590e35b375
Elves M. Rodrigues <elvesmateusrodrigues@outlook.com>
2021-09-20 11:21:31 -0300

    Setting pyppeteer version

--- src/scrapy_puppeteer/setup.py
+++ src/scrapy_puppeteer/setup.py
@@ -13,8 +13,9 @@ setuptools.setup(
     author='Rennan Cordeiro',
     author_email='rennanl@ufmg.br',
     packages=setuptools.find_packages(),
-    install_requires=['scrapy>=1.0.0', 'promise', 'pyppeteer2',
-                     'requests', 'twisted'],
+    install_requires=['scrapy>=1.0.0', 'promise', 'pyppeteer==0.2.6',
+                     'requests', 'twisted', 'python-magic',
+                     'websockets==9.1', 'pyee==8.2.2'],
     entry_points={
         'console_scripts': [
             'scrapyp = scrapy_puppeteer.cli:__main__',
