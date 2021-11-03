d3b6aaccccd8d54c1d16a7c7d9442ca9fdcfc7c5
Elves M. Rodrigues <elvesmateusrodrigues@outlook.com>
2021-11-03 08:21:44 -0300

    Preparing merge with distributed-system

--- src/scrapy_puppeteer/scrapy_puppeteer/middlewares.py
+++ src/scrapy_puppeteer/scrapy_puppeteer/middlewares.py
@@ -215,8 +215,13 @@ class PuppeteerMiddleware:
         if request.steps:
             scrshot_path = os.path.join(self.data_path, "data",
                 "screenshots", str(self.instance_id))
+
+            if not os.path.exists(scrshot_path):
+                os.makedirs(scrshot_path)
+
             steps = code_g.generate_code(request.steps, functions_file,
                 scrshot_path)
+
             request.meta["pages"] = await steps.execute_steps(pagina=page)
 
         content_type = response.headers['content-type']
@@ -231,6 +236,10 @@ class PuppeteerMiddleware:
         # Necessary to bypass the compression middleware (?)
         response.headers.pop('content-encoding', None)
         response.headers.pop('Content-Encoding', None)
+
+        print('%' * 15)
+        print(request.meta)
+        print('%' * 15)
 
         return HtmlResponse(
             page.url,
