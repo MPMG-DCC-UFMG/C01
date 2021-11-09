ff4c8ce2f00fee7485a52efd30305aae6bed08de
Elves M. Rodrigues <elvesmateusrodrigues@outlook.com>
2021-11-09 17:50:51 -0300

    Fix bad formed conditional

--- spider_manager/src/crawling/spiders/static_page.py
+++ spider_manager/src/crawling/spiders/static_page.py
@@ -206,7 +206,7 @@ class StaticPageSpider(BaseSpider):
                     message = "Not crawling links in '{}' because cur_depth={} >= maxdepth={}"
                     self._logger.debug(message.format(response.url, cur_depth, max_depth))
                     
-                if self.config.get("explore_links", False):
+                elif self.config.get("explore_links", False):
                     for link in self.extract_links(response):
                         yield Request(url=link,
                                     callback=self.parse,
