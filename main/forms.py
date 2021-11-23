49ee5bb41a3bba63f911c08c06babe3972040ed1
Elves M. Rodrigues <elvesmateusrodrigues@outlook.com>
2021-11-22 21:54:26 -0300

    Saving crawled data with dinamic encoding

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import CrawlRequest, ParameterHandler, ResponseHandler
 from django.core.exceptions import ValidationError
-
+from crawling_utils.constants import HEADER_ENCODE_DETECTION
 
 class CrawlRequestForm(forms.ModelForm):
     class Meta:
@@ -419,7 +419,7 @@ class RawCrawlRequestForm(CrawlRequestForm):
     # ENCODE DETECTION METHOD
     encoding_detection_method = forms.ChoiceField(choices=CrawlRequest.ENCODE_DETECTION_CHOICES,
                                                   label='Método de detecção de codificação das páginas',
-                                                  initial=CrawlRequest.HEADER_ENCODE_DETECTION,
+                                                  initial=HEADER_ENCODE_DETECTION,
                                                   widget=forms.RadioSelect)
 
 
--- spider_manager/src/crawling/spiders/static_page.py
+++ spider_manager/src/crawling/spiders/static_page.py
@@ -1,16 +1,17 @@
 import datetime
 import re
-import requests
 
 from scrapy.linkextractors import LinkExtractor
-from scrapy.http import Request, HtmlResponse
+from scrapy.http import Request, HtmlResponse, Response
 from scrapy_puppeteer import PuppeteerRequest
+import cchardet as chardet
 
 # Checks if an url is valid
 import validators
 
 import crawling_utils
 from crawling_utils import notify_files_found
+from crawling_utils.constants import HEADER_ENCODE_DETECTION, AUTO_ENCODE_DETECTION,AUTO_ENCODE_DETECTION_CONFIDENCE_THRESHOLD
 
 from crawling.items import RawResponseItem
 from crawling.spiders.base_spider import BaseSpider
@@ -148,7 +149,36 @@ class StaticPageSpider(BaseSpider):
 
         return set(src)
 
-    def response_to_item(self, response, files_found: set, images_found: set) -> RawResponseItem:
+    def get_encoding(self, response: Response) -> str:
+        encoding = None
+        encoding_detection_method = self.config.get('encoding_detection_method', HEADER_ENCODE_DETECTION)
+
+        if encoding_detection_method == HEADER_ENCODE_DETECTION:
+            encoding = response.encoding
+            self.logger.info(f'Encoding detected by header: {encoding}')
+
+        elif encoding_detection_method == AUTO_ENCODE_DETECTION:
+            detection = chardet.detect(response.body)
+
+            detected_encoding = detection['encoding']
+            confidence = detection['confidence']
+
+            if confidence >= AUTO_ENCODE_DETECTION_CONFIDENCE_THRESHOLD:
+                encoding = detected_encoding
+                self.logger.info(f'Encoding detected automatically: {encoding}')
+
+            else:
+                msg = f'Could not detect page encoding "{response.url}" at the level of confidence "{AUTO_ENCODE_DETECTION_CONFIDENCE_THRESHOLD}"".' + \
+                    f'The predicted encoding was "{detected_encoding}" with "{confidence}" confidence. THE PAGE WILL BE SAVED AS BINARY.'
+                self.logger.warn(msg)
+
+        else:
+            ValueError(
+                f'"{encoding_detection_method}" is not a valid encoding detection method.')
+
+        return encoding
+
+    def response_to_item(self, response: Response, files_found: set, images_found: set) -> RawResponseItem:
         item = RawResponseItem()
 
         item['appid'] = response.meta['appid']
@@ -159,7 +189,7 @@ class StaticPageSpider(BaseSpider):
         item["status_code"] = response.status
 
         item["body"] = response.body
-        item["encoding"] = response.encoding
+        item["encoding"] = self.get_encoding(response)
 
         item["referer"] = response.meta["attrs"]["referer"]
         item["content_type"] = response.headers['Content-type'].decode()
@@ -204,6 +234,7 @@ class StaticPageSpider(BaseSpider):
             if max_depth is not None and cur_depth >= max_depth:
                 message = "Not crawling links in '{}' because cur_depth={} >= maxdepth={}"
                 self._logger.debug(message.format(response.url, cur_depth, max_depth))
+
             elif self.config.get("explore_links", False):
                 for link in self.extract_links(response):
                     yield Request(url=link,
