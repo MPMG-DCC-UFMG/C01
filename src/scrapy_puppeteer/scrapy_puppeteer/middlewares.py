7d992eca98d232e12945cae6ee8faf31e1a5b59f
Elves M. Rodrigues <elvesmateusrodrigues@outlook.com>
2021-11-23 16:14:44 -0300

    Update middlewares.py

--- src/scrapy_puppeteer/scrapy_puppeteer/middlewares.py
+++ src/scrapy_puppeteer/scrapy_puppeteer/middlewares.py
@@ -132,7 +132,11 @@ class PuppeteerMiddleware:
                                         })
             await self.browser.newPage()
             page = await self.browser.newPage()
-
+            
+            # Changes the default file save location.
+            cdp = await page._target.createCDPSession()
+            await cdp.send('Browser.setDownloadBehavior', {'behavior': 'allowAndName', 'downloadPath': self.download_path})
+            
         # Cookies
         if isinstance(request.cookies, dict):
             await page.setCookie(*[
