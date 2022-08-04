0ef6983dd11a029a5230cc6a8d910a58cb7ba131
Rennan Cordeiro <rennancl@gmail.com>
2022-08-03 21:46:48 -0300

    fix install

--- clean_install.py
+++ clean_install.py
@@ -1,6 +1,8 @@
 import subprocess
 
 subprocess.run("docker-compose down".split())
-subprocess.run("docker-compose rm --volumes".split())
-subprocess.run("rm -r main/migrations/*".split())
+subprocess.run("docker-compose -f docker-compose.yml down --rmi all -v".split())
+subprocess.run("rm -r ./main/migrations".split())
+subprocess.run("mkdir -m 777 ./main/migrations".split())
+subprocess.run("touch ./main/migrations/__init__.py".split())
 subprocess.run("docker-compose build --parallel".split())
