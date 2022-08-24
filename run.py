d33a2a6dc5795c4c0f075fc0a12d0d8af4cb1510
caiocvsilva <caiocvsilva@gmail.com>
2022-08-24 16:24:43 -0400

    adicionando argumento para debugar ou nao o docker

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
 import time
 import subprocess
+import os
+import argparse
 
+parser = argparse.ArgumentParser(description='Run docker compose to build and up de container.')
+parser.add_argument('-d', '--debug', default='nodebug',
+                    help='set environment variable file used on docker compose to debug (debug) or not (nodebug) the docker execution (default: nodebug)')
+args = vars(parser.parse_args())
+
+myenv = {
+    **os.environ,
+    "DEBUGENV": str(args["debug"]),
+}
 subprocess.run("docker-compose down".split())
 subprocess.run("docker volume rm c01_static_volume".split())
-subprocess.run("docker-compose build --parallel".split())
-subprocess.run("docker-compose up -d".split())
+subprocess.run("docker-compose build --parallel".split(), env=myenv)
+subprocess.run("docker-compose up -d".split(), env=myenv)
 time.sleep(10)
 print("Finished")
