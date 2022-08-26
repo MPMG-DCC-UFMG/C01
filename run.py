34f86ca2297ec931aec2729bd6524a3aefe32414
caiocvsilva <caiocvsilva@gmail.com>
2022-08-25 20:43:26 -0400

    corrigindo warnings e errors

--- run.py
+++ run.py
@@ -3,16 +3,16 @@ import subprocess
 import os
 import argparse
 
-parser = argparse.ArgumentParser(description='Run docker compose to build and up de container.')
-parser.add_argument('-d', '--debug', default='nodebug',
-                    help='set environment variable file used on docker compose to debug (debug) or not (nodebug) the docker execution (default: nodebug)')
+parser = argparse.ArgumentParser(description='Run docker compose to build and up the container.')
+parser.add_argument('-d', '--debug', default='main',
+                    help='set environment variable file used on docker compose to debug (debug) or not (main) the docker execution (default: main)')
 args = vars(parser.parse_args())
 
 myenv = {
     **os.environ,
-    "DEBUGENV": str(args["debug"]),
+    "ENVFILENAME": str(args["debug"]),
 }
-subprocess.run("docker-compose down".split())
+subprocess.run("docker-compose down".split(), env=myenv)
 subprocess.run("docker volume rm c01_static_volume".split())
 subprocess.run("docker-compose build --parallel".split(), env=myenv)
 subprocess.run("docker-compose up -d".split(), env=myenv)
