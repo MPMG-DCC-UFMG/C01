2fb0afa598cb4cc1b1b099cbb196787cb2ce9710
caiocvsilva <caiocvsilva@gmail.com>
2023-01-20 15:39:43 -0500

    removendo arquivos antigos e desnecessarios

--- standalone-crawlers/processo-compras/compras/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Rúbia Reis Guerra
-rubia-rg@github
-Crawler for 'Processos de Compra' at https://www1.compras.mg.gov.br/processocompra/processo/consultaProcessoCompra.html
-"""
