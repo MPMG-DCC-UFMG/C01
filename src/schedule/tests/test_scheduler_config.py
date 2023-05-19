7cfac58892eccc784d874ebcee442f78636b4ab7
Elves Rodrigues <elvesmateusrodrigues@outlook.com>
2023-05-19 12:18:26 -0300

    Update test_scheduler_config.py

--- src/schedule/tests/test_scheduler_config.py
+++ src/schedule/tests/test_scheduler_config.py
@@ -1,18 +1,17 @@
 import datetime
 import unittest
 
-from constants import *
+from schedule.constants import *
 
-from scheduler_config import (SchedulerConfigDict,
-                            SchedulerConfig,
-                            SchedulerConfigInvalidRepeatModeError, 
-                            SchedulerConfigMissingFieldError, 
-                            SchedulerConfigValueError,
-                            REQUIRED_FIELDS)
+from schedule.schedule.config_dict import ConfigDict
+from schedule.schedule.config_dict import (Config,
+                            ConfigInvalidRepeatModeError, 
+                            ConfigMissingFieldError, 
+                            ConfigValueError)
 
 class TestSchedulerConfig(unittest.TestCase):
     def setUp(self) -> None:
-        self.config_dict: SchedulerConfigDict = {k: None for k in REQUIRED_FIELDS}   
+        self.config_dict: ConfigDict = {k: None for k in REQUIRED_FIELDS}   
         self._fill_start_date()
 
     def _fill_start_date(self):
@@ -30,12 +29,12 @@ class TestSchedulerConfig(unittest.TestCase):
         } 
 
     def test_raise_exception_if_missing_required_fields(self):
-        with self.assertRaises(SchedulerConfigMissingFieldError):
+        with self.assertRaises(ConfigMissingFieldError):
             for req_field in REQUIRED_FIELDS:
                 fields_with_missing_required_field = list(REQUIRED_FIELDS)
                 fields_with_missing_required_field.remove(req_field)
-                config_dict: SchedulerConfigDict = {field: None for field in fields_with_missing_required_field}   
-                SchedulerConfig.valid_config(config_dict)
+                config_dict: ConfigDict = {field: None for field in fields_with_missing_required_field}   
+                Config.valid_config(config_dict)
 
     def test_raise_exception_with_invalid_start_date(self):
         now = datetime.datetime.now()
@@ -48,89 +47,89 @@ class TestSchedulerConfig(unittest.TestCase):
 
         for invalid_input in (None, invalid_format_date, past_date_str):
             self.config_dict['start_date'] = invalid_input
-            with self.assertRaises(SchedulerConfigValueError):
-                SchedulerConfig.valid_config(self.config_dict)
+            with self.assertRaises(ConfigValueError):
+                Config.valid_config(self.config_dict)
 
     def test_raise_exception_with_invalid_repeat_mode(self):
         self.config_dict['repeat_mode'] = 'unknow_repeat_mode'
-        with self.assertRaises(SchedulerConfigInvalidRepeatModeError):
-            SchedulerConfig.valid_config(self.config_dict)
+        with self.assertRaises(ConfigInvalidRepeatModeError):
+            Config.valid_config(self.config_dict)
     
     def test_raise_if_personalized_repeat_value_is_not_dict(self):
         self.config_dict['repeat_mode'] = PERSONALIZED_REPEAT_MODE
         self.config_dict['personalized_repeat'] = None 
-        with self.assertRaises(SchedulerConfigValueError):
-            SchedulerConfig.valid_config(self.config_dict)
+        with self.assertRaises(ConfigValueError):
+            Config.valid_config(self.config_dict)
 
     def test_raise_if_personalized_repeat_is_missing_required_fields(self):
-        with self.assertRaises(SchedulerConfigMissingFieldError):
+        with self.assertRaises(ConfigMissingFieldError):
             for req_field in PERSONALIZED_REQUIRED_FIELDS:
                 fields_with_missing_required_field = list(PERSONALIZED_REQUIRED_FIELDS)
                 fields_with_missing_required_field.remove(req_field)
-                config_dict: SchedulerConfigDict = {field: None for field in fields_with_missing_required_field}   
-                SchedulerConfig.valid_config(config_dict)
+                config_dict: ConfigDict = {field: None for field in fields_with_missing_required_field}   
+                Config.valid_config(config_dict)
     
     def test_raise_if_personalized_repeat_has_invalid_repeat_interval(self):
         self._fill_personalized_repeat()
 
         self.config_dict['personalized_repeat']['interval'] = '1'
 
-        with self.assertRaises(SchedulerConfigValueError):
-            SchedulerConfig.valid_config(self.config_dict)
+        with self.assertRaises(ConfigValueError):
+            Config.valid_config(self.config_dict)
 
         self.config_dict['personalized_repeat']['interval'] = -1
-        with self.assertRaises(SchedulerConfigValueError):
-            SchedulerConfig.valid_config(self.config_dict)
+        with self.assertRaises(ConfigValueError):
+            Config.valid_config(self.config_dict)
 
     def test_raise_if_personalized_repeat_has_invalid_repeat_mode(self):
         self._fill_personalized_repeat()
 
         self.config_dict['personalized_repeat']['mode'] = 'unknow_mode'
 
-        with self.assertRaises(SchedulerConfigInvalidRepeatModeError):
-            SchedulerConfig.valid_config(self.config_dict)
+        with self.assertRaises(ConfigInvalidRepeatModeError):
+            Config.valid_config(self.config_dict)
 
     def test_raise_if_personalized_repeat_mode_has_invalid_weekly_config(self):
         self._fill_personalized_repeat()
 
         self.config_dict['personalized_repeat']['mode'] = WEEKLY_REPEAT_MODE
 
-        with self.assertRaises(SchedulerConfigValueError):
-            SchedulerConfig.valid_config(self.config_dict)
+        with self.assertRaises(ConfigValueError):
+            Config.valid_config(self.config_dict)
 
         self.config_dict['personalized_repeat']['data'] = []
 
-        with self.assertRaises(SchedulerConfigValueError):
-            SchedulerConfig.valid_config(self.config_dict)
+        with self.assertRaises(ConfigValueError):
+            Config.valid_config(self.config_dict)
 
         self.config_dict['personalized_repeat']['data'] = [7]
 
-        with self.assertRaises(SchedulerConfigValueError):
-            SchedulerConfig.valid_config(self.config_dict)
+        with self.assertRaises(ConfigValueError):
+            Config.valid_config(self.config_dict)
 
         self.config_dict['personalized_repeat']['data'] = [-1]
 
-        with self.assertRaises(SchedulerConfigValueError):
-            SchedulerConfig.valid_config(self.config_dict)
+        with self.assertRaises(ConfigValueError):
+            Config.valid_config(self.config_dict)
 
     def test_raise_if_personalized_repeat_mode_has_invalid_monthly_config(self):
         self._fill_personalized_repeat()
 
         self.config_dict['personalized_repeat']['mode'] = MONTHLY_REPEAT_MODE
-        with self.assertRaises(SchedulerConfigValueError):
-            SchedulerConfig.valid_config(self.config_dict)
+        with self.assertRaises(ConfigValueError):
+            Config.valid_config(self.config_dict)
 
         self.config_dict['personalized_repeat']['data'] = {}
-        with self.assertRaises(SchedulerConfigMissingFieldError):
-            SchedulerConfig.valid_config(self.config_dict)
+        with self.assertRaises(ConfigMissingFieldError):
+            Config.valid_config(self.config_dict)
 
         required_fields = ['mode', 'value']
         for req_field in required_fields:
             fields_with_missing_required_field = required_fields.copy()
             fields_with_missing_required_field.remove(req_field)
             self.config_dict['personalized_repeat']['data'] = {field: None for field in fields_with_missing_required_field}   
-            with self.assertRaises(SchedulerConfigMissingFieldError):
-                SchedulerConfig.valid_config(self.config_dict)
+            with self.assertRaises(ConfigMissingFieldError):
+                Config.valid_config(self.config_dict)
         
         # Personalized monthly repeat type of type DAY-X must receive a integer in the field `value` of 
         # the dict `data`, and must be between 1 and 31.
@@ -139,8 +138,8 @@ class TestSchedulerConfig(unittest.TestCase):
                 'mode': MONTHLY_DAY_X_OCCURRENCE_TYPE,
                 'value':invalid_value
             }
-            with self.assertRaises(SchedulerConfigValueError):
-                SchedulerConfig.valid_config(self.config_dict)
+            with self.assertRaises(ConfigValueError):
+                Config.valid_config(self.config_dict)
 
         # Personalized monthly repeat type of type first-weekday or last-weekday must receive a integer in the field `value` of 
         # the dict `data`, and must be between 0 and 6.
@@ -150,24 +149,24 @@ class TestSchedulerConfig(unittest.TestCase):
                     'mode': mode,
                     'value':invalid_value
                 }
-                with self.assertRaises(SchedulerConfigValueError):
-                    SchedulerConfig.valid_config(self.config_dict)
+                with self.assertRaises(ConfigValueError):
+                    Config.valid_config(self.config_dict)
 
     def test_raise_if_personalized_repeat_mode_finish_has_invalid_config(self):
         self._fill_personalized_repeat()
 
         self.config_dict['personalized_repeat']['finish'] = {}
 
-        with self.assertRaises(SchedulerConfigMissingFieldError):
-            SchedulerConfig.valid_config(self.config_dict)
+        with self.assertRaises(ConfigMissingFieldError):
+            Config.valid_config(self.config_dict)
 
         self.config_dict['personalized_repeat']['finish'] = {
             'mode': 'unknown_mode',
             'value': None
         }
 
-        with self.assertRaises(SchedulerConfigInvalidRepeatModeError):
-            SchedulerConfig.valid_config(self.config_dict)
+        with self.assertRaises(ConfigInvalidRepeatModeError):
+            Config.valid_config(self.config_dict)
 
         for invalid_input in ('-100', 0):
             self.config_dict['personalized_repeat']['finish'] = {
@@ -175,8 +174,8 @@ class TestSchedulerConfig(unittest.TestCase):
                 'value': invalid_input
             }
 
-            with self.assertRaises(SchedulerConfigValueError):
-                SchedulerConfig.valid_config(self.config_dict)
+            with self.assertRaises(ConfigValueError):
+                Config.valid_config(self.config_dict)
         
         now = datetime.datetime.now()
 
@@ -192,5 +191,5 @@ class TestSchedulerConfig(unittest.TestCase):
                 'value': invalid_input
             }
 
-            with self.assertRaises(SchedulerConfigValueError):
-                SchedulerConfig.valid_config(self.config_dict)
+            with self.assertRaises(ConfigValueError):
+                Config.valid_config(self.config_dict)
